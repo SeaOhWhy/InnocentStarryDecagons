0d0ded2a8b060e9659745a708aa3b4e0fe514419
SeaOhWhy <shahara.coy@patelhs.org>
2020-09-22 20:57:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hi friend!");
+     int myAge= 14;
+     
+     System.out.print("I am" +  myAge+ "years old."); 
+  }
+}
+          //if else statments
